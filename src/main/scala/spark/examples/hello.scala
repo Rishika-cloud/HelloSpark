32f04843626a587935ec5399486d9ba7633b95c4
Rishika-cloud <71088965+Rishika-cloud@users.noreply.github.com>
2023-01-19 14:35:25 +0530

    committing

--- src/main/scala/spark/examples/hello.scala
+++ src/main/scala/spark/examples/hello.scala
@@ -3,7 +3,7 @@ package spark.examples
 object hello extends Serializable{
   def main(args: Array[String]): Unit = {
     println("hello")
-    println("Rishika")
+    println("Rishika Rastogi")
 
   }
 
